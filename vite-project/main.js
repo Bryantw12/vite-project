a170deb83ca0a3cc33e126be7b3d8173b150f2b9
bryantw12 <b.ashton522@gmail.com>
2023-12-07 12:14:05 -0500

    Deployment

---
+++ vite-project/main.js
@@ -0,0 +1,32 @@
+import './style.css'
+import{getEl} from './utils.js';
+
+const handleSubmit = (e) =>{
+  e.preventDefault();
+  const formObj= Object.fromEntries(new FormData(e.target))
+  console.log(formObj)
+}
+
+const getLocalStorageKey = (key) =>{
+  try{
+    return JSON.parse(localStorage.getItem(key));
+  } catch(err){
+    console.error(err)
+    return null
+  }
+}
+
+export const getValue = (key) => getLocalStorageKey(key);
+export const setValue = (key,names) => setLocalStorageKey(key,names)
+
+const addValue = (value) =>{
+  const values = getValue('form-data');
+  setValue('form-data', [...values,value])
+
+}
+
+const main = () => {
+  getEl('#palette-picker').addEventListener('submit', handleSubmit)
+}
+
+main; 
